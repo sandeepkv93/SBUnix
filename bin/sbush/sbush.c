0f7f4cf7f72ff39ce1c3fd0fe1b0ee266361f149
Raveendra Soori <sooriravindra@gmail.com>
2017-09-01 11:33:15 -0400

    Added comments, added new makefile

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -4,7 +4,17 @@
 
 int main(int argc, char *argv[], char *envp[]) {
 	while (getchar()) {
-    		puts("sbush> ");
-  	}
-  	return 0;
+		// Print PS1.
+		puts("sbush> ");
+
+		/*
+		 * Maintain PATH
+		 * Read the args and run script, else:
+		 * 1. Check for built in commands
+		 * 2. Check for |, split into commands
+		 * 3. Check for &, set BG
+		 * 4. Fork and exec binary. check BG
+		 */
+	}
+	return 0;
 }
