d2d7d4aaa352abd8528b3c3032cd2a833e164cf6
Sandeep K V <sandeepkv93@gmail.com>
2017-08-31 22:29:40 -0400

    Indenting the sbush.c

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-  while (getchar()) {
-    puts("sbush> ");
-  }
-  return 0;
+	while (getchar()) {
+    		puts("sbush> ");
+  	}
+  	return 0;
 }
