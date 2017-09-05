b00d57a09aaceb5e38258dd37638fe369ef6ff98
Sandeep K V <sandeepkv93@gmail.com>
2017-09-05 18:49:12 -0400

    ls command with system calls -- from backup

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -1,12 +1,20 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/syscall.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
-int BUF_SIZE = 255;
-int FORMATTING_VAR_SIZE = 10;
+#define BUF_SIZE 1024
+#define CONSOLE_BUF_SIZE 255
+#define FORMATTING_VAR_SIZE 10
+
+struct new_dirent {
+    long inode_num;
+    off_t d_off;
+    unsigned short d_reclen;
+    char d_name[];
+};
 
 int string_write(int fd,char * console_buffer, int buffer_size)
 {
@@ -18,50 +26,76 @@ int main(int argc, char* argv[])
 {
     struct dirent *dp;
     struct stat filestats;
-    DIR *directory;
+    int fd;
+    int nread;
+    char dirent_buffer[BUF_SIZE];
+    struct new_dirent *new_dirent_structure;
+    int directory_position;
     char *file_name;    
-    char console_buffer[BUF_SIZE];
-    char stat_buf[BUF_SIZE];
+    char console_buffer[CONSOLE_BUF_SIZE];
+    char stat_buf[CONSOLE_BUF_SIZE];
     char formatter[FORMATTING_VAR_SIZE];
     char *format_unset = "\e[0m\e[21m  ";
     char *directory_name = (argc>1)?argv[1]:".";
     char *new_line_buffer = "\n";
-    directory = opendir(directory_name);
-    while ((dp=readdir(directory)) != NULL) {
-        if (strcmp(dp->d_name, "..") && strcmp(dp->d_name, ".")) {
-            file_name = dp->d_name;
+    char error_message[CONSOLE_BUF_SIZE];
 
-            /* Write directory_name and file_name in the form, directory_name/file_name */
-            strcpy(stat_buf,directory_name);
-            strcat(stat_buf,"/");
-            strcat(stat_buf,file_name);
+    /* Open the directory*/
+    fd = open(directory_name, O_RDONLY | O_DIRECTORY);    
+    while(1) {
+        nread = syscall(SYS_getdents, fd, dirent_buffer, BUF_SIZE);
+        if (nread == -1) {
+            strcpy(error_message,"ls: cannot access \'");
+            strcat(error_message,directory_name);
+            strcat(error_message,"\': No such file or directory\n");
+            string_write(1, error_message, strlen(error_message));
+            _exit(-1);
+        }
+
+        /* read till nread becomes zero*/
+        if (nread == 0)
+            break;
+
+        for (directory_position = 0; directory_position < nread;directory_position += new_dirent_structure->d_reclen) {
+            new_dirent_structure = (struct new_dirent *) (dirent_buffer + directory_position);
             
-            /* Fetch the file stat into filestats struct via stat system call*/
-            stat(stat_buf, &filestats);
+            /* Get the file name from dirent structure */
+            file_name = new_dirent_structure->d_name;
+            
+            if (strcmp(file_name, "..") && strcmp(file_name, ".")) {
 
-            /* Formatting the filenames based on the type of files*/
-            if (S_ISDIR(filestats.st_mode)) {
-                strcpy(formatter,"\e[1m\e[34m");
-            }
-            else if (S_ISLNK(filestats.st_mode)) {
-                strcpy(formatter, "\e[1m\e[94m");
-            }
-            else if(filestats.st_mode & S_IXUSR) {
-                strcpy(formatter, "\e[1m\e[32m");
-            }
-            else {
-                strcpy(formatter,"");
-            }
+                /* Write directory_name and file_name in the form, directory_name/file_name */
+                strcpy(stat_buf,directory_name);
+                strcat(stat_buf,"/");
+                strcat(stat_buf,file_name);
+                
+                /* Fetch the file stat into filestats struct via stat system call*/
+                stat(stat_buf, &filestats);
 
-            /* Write to the formatted string to the buffer */
-            strcpy(console_buffer,formatter);
-            strcat(console_buffer,file_name);
-            strcat(console_buffer,format_unset);
+                /* Formatting the filenames based on the type of files*/
+                if (S_ISDIR(filestats.st_mode)) {
+                    strcpy(formatter,"\e[1m\e[34m");
+                }
+                else if (S_ISLNK(filestats.st_mode)) {
+                    strcpy(formatter, "\e[1m\e[94m");
+                }
+                else if(filestats.st_mode & S_IXUSR) {
+                    strcpy(formatter, "\e[1m\e[32m");
+                }
+                else {
+                    strcpy(formatter,"");
+                }
 
-            string_write(1,console_buffer,strlen(console_buffer));
+                /* Write to the formatted string to the buffer */
+                strcpy(console_buffer,formatter);
+                strcat(console_buffer,file_name);
+                strcat(console_buffer,format_unset);
+
+                string_write(1,console_buffer,strlen(console_buffer));
+            }            
         }
     }
-    closedir(directory);
+    close(fd);
     string_write(1,new_line_buffer,strlen(new_line_buffer));
     return 0;
 }
