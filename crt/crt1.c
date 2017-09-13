7ae8a3e9ec45ed2df652c357b48b9bc0d0239eef
Raveendra Soori <sooriravindra@gmail.com>
2017-09-12 22:06:47 -0400

    Fix the _start function in libc

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long __argc;
-char* __argv;
-char a[2];
-void
-fix_my_stack(void)
-{
-    // This fixes the stack! lol
-}
 void
 _start(void)
 {
-    __asm__("popq %%rsi;"
-            "popq %%rsi;"
+    long __argc;
+    char* __argv;
+    __asm__("movq %%rsp, %%rdx;"
             "popq %%rsi;"
             "popq %%rsi;"
             "movq %%rsi, %0;"
             "movq %%rsp, %1;"
+            "movq %%rdx, %%rsp"
             : "=r"(__argc), "=r"(__argv)
             :
-            : "%rsi", "%rsp");
-    fix_my_stack();
+            : "%rsi", "%rsp", "%rdx");
     exit(main(__argc, (char**)__argv, (char**)((char**)__argv + __argc + 1)));
 }
+
+/*
+
+------------------------- Higher MEM
+|.          |
+|-----------|
+|NULL       |
+|---        |
+|.          |
+|---        |
+|.          |
+|---        |
+|envp 1     |
+|---        |
+|envp 0     |
+|-----------|
+|NULL       |
+|----       |
+|argv n-1   |
+|---        |  |
+|.          |  | Stack grows this way
+|---        |  V
+|.          |
+|---        |
+|argv 3     |
+|---        |
+|argv 1     |
+|---        |
+|argv 0     |
+|-----------|
+|argc = n   |
+|---<<<<<<<<<<<<<< RSP --- Lower MEM
+
+*/
