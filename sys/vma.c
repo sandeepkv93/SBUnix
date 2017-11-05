4300c79a4d717a51e0a1daf515a184e4e591a1f0
Raveendra Soori <sooriravindra@gmail.com>
2017-11-05 15:17:16 -0500

    Adds function to create VA to physical address maping

--- sys/vma.c
+++ sys/vma.c
@@ -6,6 +6,7 @@
 #define PAGELIST_ENTRIES (1024 * 1024)
 #define PAGE_SIZE 4096
 #define TABLE_ENTRIES 512
+#define VMA_PML4_VADDR 0x110000
 
 #define VMA_PHYSICAL_PAGE_OFFSET(x) (x & 0xfff)
 #define VMA_PAGE_TABLE_OFFSET(x) ((x >> 12) & 0x1ff)
@@ -18,6 +19,8 @@ uint64_t cur_kern_heap = VMA_VIDEO + 0x10000;
 struct pagelist_t pages[PAGELIST_ENTRIES];
 struct pagelist_t* freepage_head;
 extern void paging_enable(void*);
+uint64_t test_address;
+#define TEST_VMA (VMA_KERNMEM + 0x801000)
 
 void
 vma_pagelist_add_addresses(uint64_t start, uint64_t end)
@@ -69,7 +72,7 @@ vma_pagelist_getpage()
 }
 
 uint64_t*
-vma_add_table_mapping(uint64_t* table, uint32_t offset)
+vma_get_table_entry(uint64_t* table, uint32_t offset)
 {
     // Adds entry into offset of the table if not present, returns entry
     char* temp_byte;
@@ -85,13 +88,37 @@ vma_add_table_mapping(uint64_t* table, uint32_t offset)
 }
 
 void
+vma_add_pagetable_mapping(uint64_t* pml4_phys_addr, uint64_t v_addr,
+                          uint64_t phys_entry)
+{
+    uint64_t* pdp_table;
+    uint64_t* pd_table;
+    uint64_t* pt_table;
+
+    pdp_table = vma_get_table_entry(pml4_phys_addr, VMA_PML4_OFFSET(v_addr));
+
+    pd_table = vma_get_table_entry(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
+
+    pt_table = vma_get_table_entry(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
+
+    pt_table[VMA_PAGE_TABLE_OFFSET(v_addr)] = phys_entry;
+}
+
+void
+testing_function()
+{
+    test_address = (uint64_t)vma_pagelist_getpage();
+    uint8_t* test_ptr = (uint8_t*)test_address;
+    for (int i = 0; i < 1024; i++) {
+        test_ptr[i] = i;
+    }
+}
+
+void
 vma_create_pagetables()
 {
     // Creates the 4 level pagetables needed and switches CR3
     uint64_t* pml4_table = vma_pagelist_getpage();
-    uint64_t* pdp_table;
-    uint64_t* pd_table;
-    uint64_t* pt_table;
     uint64_t v_addr;
 
     for (int i = 0; i < TABLE_ENTRIES; i++) {
@@ -104,40 +131,34 @@ vma_create_pagetables()
     // TODO Remove this hard coding of 2048. Map only physbase to physfree
     for (int i = 0; i < 4735; i++) {
         v_addr = VMA_KERNMEM + i * (PAGE_SIZE);
-        pdp_table = vma_add_table_mapping(pml4_table, VMA_PML4_OFFSET(v_addr));
-        pd_table =
-          vma_add_table_mapping(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
-        pt_table =
-          vma_add_table_mapping(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
-
-        pt_table[VMA_PAGE_TABLE_OFFSET(v_addr)] = (0x0 + i * (PAGE_SIZE)) | 0x3;
+        vma_add_pagetable_mapping(pml4_table, v_addr, (i * PAGE_SIZE) | 0x3);
     }
-    // TODO add new function to create the mapping making the following code
-    // generic
-    v_addr = VMA_VIDEO;
-    pdp_table = vma_add_table_mapping(pml4_table, VMA_PML4_OFFSET(v_addr));
-    pd_table = vma_add_table_mapping(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
-    pt_table =
-      vma_add_table_mapping(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
-    pt_table[VMA_PAGE_TABLE_OFFSET(v_addr)] = (0xb8000) | 0x3;
 
-    v_addr = 0x110000;
-    pdp_table = vma_add_table_mapping(pml4_table, VMA_PML4_OFFSET(v_addr));
-    pd_table = vma_add_table_mapping(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
-    pt_table =
-      vma_add_table_mapping(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
-    pt_table[VMA_PAGE_TABLE_OFFSET(v_addr)] =
-      (uint64_t)((uint64_t)pml4_table | 0x3);
+    // testing_function();
+
+    vma_add_pagetable_mapping(pml4_table, VMA_VIDEO, (0xb8000) | 0x3);
+    /*vma_add_pagetable_mapping(pml4_table, TEST_VMA, (test_address) | 0x3);*/
+    vma_add_pagetable_mapping(pml4_table, VMA_PML4_VADDR,
+                              ((uint64_t)pml4_table) | 0x3);
+
     paging_enable(pml4_table);
     kprintf("end of page create func!");
-    uint64_t p = (uint64_t)(v_addr);
-    // uint64_t* p = (uint64_t*)0xffffffffffff;
-    kprintf("\n%x\n", VMA_PML4_OFFSET(p));
-    kprintf("\n%x\n", VMA_PD_POINTER_OFFSET(p));
-    kprintf("\n%x\n", VMA_PAGE_DIRECTORY_OFFSET(p));
-    kprintf("\n%x\n", VMA_PAGE_TABLE_OFFSET(p));
+
+    // TESTING CODE -- Delete after understanding
+    uint64_t p = (uint64_t)(VMA_PML4_VADDR);
     uint64_t* ptr = (uint64_t*)p;
     kprintf("\n value at pml4[0] %x\n", *ptr);
+    kprintf("\n value at pml4[510] %x\n", ptr[510]);
+    kprintf("\n value at pml4[511] %x\n", ptr[511]);
+    kprintf("\n value of pml4_phys %x\n", pml4_table);
+
+    /*
+    kprintf("Testpage is at physical %x", test_address);
+    uint8_t* test_ptr = (uint8_t*)TEST_VMA;
+    for (int k = 0; k < 20; k++) {
+        kprintf("%d ", test_ptr[k]);
+    }
+    */
 }
 
 void*
@@ -154,10 +175,9 @@ get_free_pages()
     v_addr = cur_kern_heap;
     kprintf("%p \n%p", phy_addr, v_addr);
     cur_kern_heap += PAGE_SIZE;
-    pdp_table = vma_add_table_mapping(pml4_table, VMA_PML4_OFFSET(v_addr));
-    pd_table = vma_add_table_mapping(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
-    pt_table =
-      vma_add_table_mapping(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
+    pdp_table = vma_get_table_entry(pml4_table, VMA_PML4_OFFSET(v_addr));
+    pd_table = vma_get_table_entry(pdp_table, VMA_PD_POINTER_OFFSET(v_addr));
+    pt_table = vma_get_table_entry(pd_table, VMA_PAGE_DIRECTORY_OFFSET(v_addr));
     pt_table[VMA_PAGE_TABLE_OFFSET(v_addr)] = (uint64_t)(phy_addr) | 0x3;
     return (void*)v_addr;
 }
