a07ff5b656c92842eeaabc9ebac6d41094ec8bb3
Raveendra Soori <sooriravindra@gmail.com>
2017-11-09 04:54:21 -0500

    Fixed register value changing in ISR bug

--- sys/isr.s
+++ sys/isr.s
@@ -2,7 +2,8 @@
 .extern timer_isr, kb_isr 
 
 push_regs:
-    pop %r8
+
+    // Push all regs onto stack
     push %rsi
     push %rdi
     push %rbp
@@ -11,9 +12,40 @@ push_regs:
     push %rcx
     push %rdx
     push %r8
+    push %r9
+    push %r10
+    push %r11
+    push %r12
+    push %r13
+    push %r14
+    push %r15
+
+    /* Since we changed the stack we need to push the return address 
+     * which was pushed onto the stack when this function was called. 
+     * First read it from stack, then push. 120 = 8(size) * 15(num of regs) */
+    movq 120(%rsp), %rax
+    push %rax
+
     retq
 
 pop_regs:
+
+    /* 
+     * The stack top has return address. We move it to the the location that 
+     * will be the stack top once we pop all registers. This allows to safely 
+     * return to the place that this function was called from.
+     */
+    pop %rax
+    movq %rax,120(%rsp)
+
+    // Now we pop all regs
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
+    pop %r11
+    pop %r10
+    pop %r9
     pop %r8
     pop %rdx
     pop %rcx
@@ -22,7 +54,6 @@ pop_regs:
     pop %rbp
     pop %rdi
     pop %rsi
-    push %r8
     retq
 
 timer_isr_asm:
