12ff0d9ff0a9b3c68cc97580d83e005014bd4c82
Sandeep K V <sandeepkv93@gmail.com>
2017-12-05 04:46:35 -0500

    PWD, CD, GETENV, SETENV

--- libc/string.c
+++ libc/string.c
@@ -56,6 +56,20 @@ strcmp(const char* s1, const char* s2)
 }
 
 int
+strncmp(const char* s1, const char* s2, int n)
+{
+    while (n > 0 && *s1 == *s2 && *s1 != '\0') {
+        s1++;
+        s2++;
+        n--;
+    }
+    if (n == 0) {
+        return 0;
+    }
+    return *s1 - *s2;
+}
+
+int
 strlen(const char* s1)
 {
     int len = 0;
