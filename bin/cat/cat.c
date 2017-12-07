2b52313652f1a32bb244408042b6941d68af0559
Sandeep K V <sandeepkv93@gmail.com>
2017-12-07 00:40:03 -0500

    Code Cleanup of Dead and Unnecessary Code

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -3,24 +3,6 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-/*
-int open_new(char * filename, int flags)
-{
-        return syscall(SYS_open,filename,flags);
-}
-
-int read_new(int fd,char * buf, int size)
-{
-        //return depends on the system call. The size read will be returned
-unless there is an error. 0 on EOF.
-        return syscall(SYS_read,fd,buf,size);
-}
-int write_new(int fd,char * buf, int size)
-{
-        return syscall(SYS_write,fd,buf,size);
-}
-*/
-
 int BUF_SIZE = 255;
 int
 main(int argc, char** argv)
